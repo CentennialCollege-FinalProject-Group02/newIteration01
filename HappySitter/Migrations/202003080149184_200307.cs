cbce16f0b0452626a7efc902318f467e61ac3160
Kwanghun Ko <khyjs80@gmail.com>
2020-03-13 20:18:48 -0400

    1 Initial Setup,  Registration for users. Role Feature was added.

---
+++ HappySitter/Migrations/202003080149184_200307.cs
@@ -0,0 +1,16 @@
+namespace HappySitter.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class _200307 : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
